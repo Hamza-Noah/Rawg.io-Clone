fb4a7d669b58d80fa922cac54f2476917707bb40
Hamza-Noah <hamza.nouh97@gmail.com>
2024-09-18 09:59:34 +0300

    Install Chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button colorScheme="blue">Button</Button>
+    </>
+  );
 }
 
 export default App;
