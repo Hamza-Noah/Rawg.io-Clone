013a8e8736f69ed2c641889756576e746f549f47
Hamza-Noah <hamza.nouh97@gmail.com>
2024-09-19 04:16:39 +0300

    bullied the color mode switch

---
+++ src/Components/ColorModeSwitch.tsx
@@ -0,0 +1,15 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <>
+      <HStack justifyContent="space-between" padding="10px">
+        <Switch colorScheme="green" isChecked={colorMode === "dark"}  onChange={toggleColorMode}/>
+        <Text>Color Mode</Text>
+      </HStack>
+    </>
+  );
+};
+
+export default ColorModeSwitch;
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,11 +1,14 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 const Navbar = () => {
-  return <HStack>
-    <Image boxSize="60px" src={logo}/>
-    <Text />
-  </HStack>;
+  return (
+    <HStack>
+      <Image boxSize="60px" src={logo} />
+      <ColorModeSwitch />
+    </HStack>
+  );
 };
 
 export default Navbar;
