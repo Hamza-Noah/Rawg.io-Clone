947e2ed7efeea87a9e66004ae52997603a0a7a4a
Hamza-Noah <hamza.nouh97@gmail.com>
2024-09-30 16:12:55 +0300

    display genres

--- src/Components/GenreList.tsx
+++ src/Components/GenreList.tsx
@@ -1,14 +1,25 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
 
   return (
-    <ul>
+    <List>
       {data.map((genre, i) => (
-        <li key={i}>{genre.name}</li>
+        <ListItem key={i} paddingY="5px ">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
